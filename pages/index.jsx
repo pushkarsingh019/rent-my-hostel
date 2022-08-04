0e4aa4cdbdcfb8af70562ff9a3decd5b1dfa3a24
Pushkar Singh <stoazest@gmail.com>
2022-08-04 15:15:07 +0530

    added apostrophe

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,23 +4,23 @@ import Footer from "../components/Footer";
 
 function Hero(){
   return(
-    <main className="hero">
-      <h1>Find Pg's in Ahmedabad</h1>
+    <section className="hero">
+      <h1>Find Pg&apos;s in Ahmedabad</h1>
       <br />
       <p>Rent my Hostel helps you by finding the available pgs & Hostels near Ahmedabad University.</p>
       <br />
       <br />
       <Button variant={`button cta`} link={`/home`} text="Find Pg's Near Ahmedabad University" />
-    </main>
+    </section>
   )
 }
 
 export default function Home(){
   return(
-      <>
+      <main>
         <Navbar />
         <Hero />
         <Footer />
-      </>
+      </main>
   )
 }
